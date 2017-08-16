754d97fafa3e8eb1cf512c875ec99e2bd3773f36
Alexander Serebriyan <ekefar@gmail.com>
2017-08-17 02:36:09 +0300

    Has cycle - fast/slow pointers solution.

--- src/main/java/lists/HasCycle.java
+++ src/main/java/lists/HasCycle.java
@@ -25,6 +25,21 @@ public class HasCycle {
         return false;
     }
 
+    boolean hasCycleFastSlow(Node head) {
+        if (head == null) return false;
+
+        Node slow = head;
+        Node fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) return false;
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+
     class Node {
         int data;
         Node next;
