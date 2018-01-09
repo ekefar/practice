7c415c46d51f76a5a791e02b253275bec0292ef2
Alexander Serebriyan <ekefar@gmail.com>
2018-01-10 01:07:01 +0200

    Add description for moving average problem.

--- src/main/java/general/MovingAverage.java
+++ src/main/java/general/MovingAverage.java
@@ -5,6 +5,17 @@ import java.util.Queue;
 
 /**
  * @author : Alexander Serebriyan
+ *         <p>
+ *         https://leetcode.com/problems/moving-average-from-data-stream/
+ *         <p>
+ *         Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+ *         <p>
+ *         For example,
+ *         MovingAverage m = new MovingAverage(3);
+ *         m.next(1) = 1
+ *         m.next(10) = (1 + 10) / 2
+ *         m.next(3) = (1 + 10 + 3) / 3
+ *         m.next(5) = (10 + 3 + 5) / 3
  */
 public class MovingAverage {
 
