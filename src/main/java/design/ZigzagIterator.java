be8733a3385c4c9d17f859744999497d570ca1ec
Alexander Serebriyan <ekefar@gmail.com>
2018-02-20 04:36:43 +0200

    ZigZag iterator input validation.

--- src/main/java/design/ZigzagIterator.java
+++ src/main/java/design/ZigzagIterator.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 /**
  * @author : Alexander Serebriyan
+ * https://leetcode.com/problems/zigzag-iterator/description/
  */
 public class ZigzagIterator {
 
@@ -11,39 +12,49 @@ public class ZigzagIterator {
     Iterator<Integer> current;
 
     public static void main(String[] args) {
-        new ZigzagIterator(new ArrayList<>(), new ArrayList<>());
+        final ArrayList<Integer> v1 = new ArrayList<>();
+        v1.add(1);
+        v1.add(2);
 
+        final ArrayList<Integer> v2 = new ArrayList<>();
+        v2.add(3);
+        v2.add(4);
+        v2.add(5);
+        v2.add(6);
+        final ZigzagIterator iterator = new ZigzagIterator(v1, v2);
+
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
     }
 
 
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-        chain.add(v1.iterator());
-        chain.add(v2.iterator());
+        if (!v1.isEmpty()) {
+            chain.add(v1.iterator());
+        }
+        if (!v2.isEmpty()) {
+            chain.add(v2.iterator());
+        }
 
-        moveNext();
+        current = chain.poll();
     }
 
     public int next() {
+
         final Integer res = current.next();
 
-        if(current.hasNext()) {
+        if (current.hasNext()) {
             chain.add(current);
         }
 
-        moveNext();
-
+        current = chain.poll();
         return res;
     }
 
-    private void moveNext() {
-        current = null;
-        while (current == null && chain.size() > 0) {
-            current = chain.poll();
-            if(!current.hasNext()) {
-                current = null;
-            }
-        }
-    }
 
     public boolean hasNext() {
         return current != null && current.hasNext();
