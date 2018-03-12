09d6e6d8f9c846ccca5726497ab2ea828d5dc625
Alexander Serebriyan <ekefar@gmail.com>
2018-03-12 03:37:00 +0200

    Reverse linked list.

---
+++ src/main/java/lists/ReverseLinkedList.java
@@ -0,0 +1,46 @@
+package lists;
+
+/**
+ * @author : Alexander Serebriyan
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ */
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+        final ListNode a = new ListNode(1);
+        final ListNode b = new ListNode(2);
+        a.next = b;
+
+        final ListNode listNode = reverseList(a);
+        System.out.println(listNode);
+    }
+
+
+    public static ListNode reverseList(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+
+        ListNode previous = head;
+        ListNode current = head.next;
+        previous.next = null;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
