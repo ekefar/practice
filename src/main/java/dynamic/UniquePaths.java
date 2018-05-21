dc6e6359669450cd9f9870d7a4a65757426ec133
Alexander Serebriyan <ekefar@gmail.com>
2018-05-21 04:39:27 +0300

    UniquePaths.java

---
+++ src/main/java/dynamic/UniquePaths.java
@@ -0,0 +1,31 @@
+package dynamic;
+
+/**
+ * @author : Alexander Serebriyan
+ */
+public class UniquePaths {
+
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(7,3));
+    }
+
+    public static int uniquePaths(int m, int n) {
+        int[][] grid = new int[m][n];
+
+        grid[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i +1 < m) {
+                    grid[i+1][j] += grid[i][j];
+                }
+
+                if(j+1 < n) {
+                    grid[i][j+1] += grid[i][j];
+                }
+            }
+        }
+
+        return grid[m-1][n-1];
+    }
+}
