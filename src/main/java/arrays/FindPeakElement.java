316dbb4c4d859d0ea41fbe1b8f83da9ac232957d
Alexander Serebriyan <ekefar@gmail.com>
2018-08-13 02:03:00 +0300

    Find peak element.

---
+++ src/main/java/arrays/FindPeakElement.java
@@ -0,0 +1,31 @@
+package arrays;
+
+/**
+ * @author : Alexander Serebriyan
+ * https://leetcode.com/problems/find-peak-element/description/
+ */
+public class FindPeakElement {
+
+    public static void main(String[] args) {
+        System.out.println(findPeakElement(new int[] {1}));
+    }
+
+    public static int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length -1 ;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(mid > left && nums[mid-1] >= nums[mid]) {
+                right = mid - 1;
+            } else if(mid < right && nums[mid+1] >= nums[mid]) {
+                left = mid +1;
+            } else {
+                return mid;
+            }
+        }
+
+        return 0;
+    }
+}
