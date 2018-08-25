a1dcad34f4a7d912ff36bdaa9aa25d6787a90083
Alexander Serebriyan <ekefar@gmail.com>
2018-08-25 21:44:53 +0300

    Root to leaf path sum.

---
+++ src/main/java/tree/PathSum.java
@@ -0,0 +1,41 @@
+package tree;
+
+/**
+ * @author : Alexander Serebriyan
+ */
+public class PathSum {
+
+    public static void main(String[] args) {
+        TreeNode root = TreeFactory.getSimpleTree();
+
+        System.out.println(hasPathSum(root, 1));
+        System.out.println(hasPathSum(root, 3));
+        System.out.println(hasPathSum(root, 4));
+        System.out.println(hasPathSum(root, 7));
+        System.out.println(hasPathSum(root, 17));
+        System.out.println(hasPathSum(root, 0));
+    }
+
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        return hasPathSum(root, 0, sum);
+    }
+
+    public static boolean hasPathSum(TreeNode root, int currentSum, int target) {
+
+        if (root == null) {
+            return false;
+        }
+
+        int sum = root.val + currentSum;
+        if (sum == target) {
+            return true;
+        }
+
+        if (sum < target) {
+            return hasPathSum(root.left, sum, target) ||
+                   hasPathSum(root.right, sum, target);
+        }
+
+        return false;
+    }
+}
